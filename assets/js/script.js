cbcbb472f6f786225155f855ba56adcc3307f94d
Josefina <134737124+Monroestein@users.noreply.github.com>
2024-08-31 00:31:23 -0400

    now it's prettier but still won't work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
+
+// Getting data from API
+
 function getData(idCharacter) {
     let promise = fetch("https://swapi.dev/api/people/" + idCharacter)
 
     promise
         .then(response => {
+
             console.log(response)
+
             if (!response.ok) {
+
                 if (response.status === 404) {
+
                     alert(`Character not found.`)
                     throw new Error(`Resource not found.`)
+
                 } else {
+
                     console.error(`HTTP Error`, response.status)
                     throw new Error(`Couldn't obtain character.`)
                 }
@@ -26,21 +35,37 @@ function getData(idCharacter) {
         })
 }
 
+// Injecting data to HTML.
+//      ! Fix the first going last when injecting other cards.
+
 function injectInfo(character) {
 
     const {name, height, mass} = character;
 
-    let infoHTML = ` <div class="card">
-                        <h3>${name}</h3>
-                        <p>Height: ${height} | Mass: ${mass} kg.</p>
-                    </div>`;
+    let infoHTML = ` 
+    <div class="card">
+        <h3>${name}</h3>
+        <p>Height: ${height} m. | Mass: ${mass} kg.</p>
+    </div>
+    `;
 
     console.log(infoHTML)
 
-    document.getElementById('mostPopular').innerHTML = infoHTML;
+    document.getElementById('mostPopular').innerHTML += infoHTML;
  }
 
+// I need to add event listener to button
 
-getData(4)
+    // document.querySelector('.numbers').addEventListener('hover', injectInfo)
 
-// 1:36:58
+
+// I need to make a loop until getData reaches 5
+
+// getData(1)
+// getData(6)
+
+/*
+
+*/
+
+// 1:41:58
