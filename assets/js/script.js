b934e3fc6df2a5a4455678ae6ec1cf0a24bf904e
Josefina <134737124+Monroestein@users.noreply.github.com>
2024-09-07 17:35:12 -0400

    getting there

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,6 @@
+// Getting data from API 
 
-// Getting data from API and returns it
-
-async function fetchingData (id) {
-
-    let id = 3;
+async function fetchData (id) {
     
     try {
 
@@ -13,28 +10,28 @@ async function fetchingData (id) {
 
             if (response.status === 404){
 
-                alert(`Character not found.`)
+                alert('Character not found.')
 
-                console.error(`Character not found.`, response.status)
+                console.error('Character not found.', response.status)
 
-                throw new Error(`Resource not found.`)
+                throw new Error('Resource not found.')
 
             }
 
             else {
 
-                console.error(`HTTP Error`, response.status)
+                console.error('HTTP Error', response.status)
 
-                throw new Error(`Unable to get character.`)
+                throw new Error('Unable to get character.')
 
             }
         }
 
         const data = await response.json(); // Returns a promise so we use await
-
+        
         console.log(data)
-
-        injectingCard(data)
+        
+        injectCard(data, id)
 
     }
 
@@ -46,11 +43,9 @@ async function fetchingData (id) {
 
 };
 
-
-
 // Injecting card into html
 
-function injectingCard(character){
+function injectCard(character, id){
     
     const {name, height, mass} = character
 
@@ -61,29 +56,57 @@ function injectingCard(character){
                     </div>
     `;
 
-    if (id <= 5){
+    if (id <= 5) { 
 
-        document.getElementById('mostPopular').innerHTML += cardHTML;
-
+        document.querySelector("#numPop").addEventListener('mouseover', function(){
+            document.getElementById('mostPopular').innerHTML += cardHTML;
+        })
+        
     } else if (id > 5 && id <= 10) {
+ 
+        document.querySelector("#numSec").addEventListener('mouseover', function(){
+            document.getElementById('secondary').innerHTML += cardHTML;
+        })
 
-        document.getElementById('secondary').innerHTML += cardHTML;
+    } else if (id > 10 && id <= 15){
 
-    } else if (id >= 15){
-
-        document.getElementById('others').innerHTML += cardHTML;
+        document.querySelector("#numThi").addEventListener('mouseover', function(){
+            document.getElementById('others').innerHTML += cardHTML;
+        })
 
     } else {
 
-        console.error(`Unable to show character.`)
+        console.error('Unable to show character.')
 
     }
 
-    console.log(cardHTML)
+    //console.log(cardHTML)
 
 };
 
 
+/* 
+    - Each iteration have to add to the id
+    - It can't show more than five cards.
+*/
+
+// Increase id and limit iterations to 15.
+
+function limitIterations(){
+
+    let count = 1
+
+    for (let i = 1; i <= 15; i++){
+        fetchData(count)
+        count++
+    }
+
+    return
+    //console.log(count)
+}
+
+
+limitIterations()
 
 
 
