8eab86d6dfce3533977c19c63d2a8cd5f469be2b
Josefina <134737124+Monroestein@users.noreply.github.com>
2024-09-30 14:52:34 -0300

    may the force be with me

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,111 +1,86 @@
+// let count = 1
+
 // Getting data from API 
 
-async function fetchData(id) {
+async function fetchData(id, id_dom) {
 
     try {
-
         let response = await fetch("https://swapi.dev/api/people/" + id)
-
         if (!response.ok) {
-
             if (response.status === 404) {
-
                 alert('Character not found.')
                 console.error('Character not found.', response.status)
                 throw new Error('Resource not found.')
-
-            }
-
-            else {
-
+            } else {
                 console.error('HTTP Error', response.status)
                 throw new Error('Unable to get character.')
-
             }
         }
-
         const data = await response.json(); // Returns a promise so we use await
         console.log(data)
-        injectCard(data, id)
-
-    }
-
-    catch (error) {
-
+        injectCard(data, id_dom)
+        return true
+    } catch (error) {
         console.error(error)
-
     }
-
 };
 
 // Injecting card into html
 
 function injectCard(character, id) {
-
     const { name, height, mass } = character
-
     let cardHTML = `
                     <div class="card">
                         <h3>${name}</h3>
                         <p>Height: ${height} m. | Mass: ${mass} kg.</p>
-                    </div>
-    `;
+                    </div>`;
 
-    if (id <= 5) {
+    document.getElementById(id).innerHTML += cardHTML;
 
-        document.querySelector("#numPop").addEventListener('click', function () {
-            document.getElementById('mostPopular').innerHTML += cardHTML;
-        })
-
-
-    } else if (id > 5 && id <= 10) {
-
-        document.querySelector("#numSec").addEventListener('click', function () {
-            document.getElementById('secondary').innerHTML += cardHTML;
-        })
-
-    } else if (id > 10 && id <= 15) {
-
-        document.querySelector("#numThi").addEventListener('click', function () {
-            document.getElementById('others').innerHTML += cardHTML;
-        })
-
-    } else {
-
-        console.error('Unable to show character.')
-
-    }
-
-    // console.log(limitIterations)
 };
-/* 
-    - Each iteration has to add to the id
-    - It can't show more than five cards.
-*/
 
-// Increase id and limit iterations to 15.
+/* Primary */
 
-function limitIterations() {
+let countPrimary = 1
 
-    let count = 1
+document.getElementById('numPop').addEventListener('click', function (){
+    
+    if(countPrimary <= 5){
 
-    for (let i = 1; i <= 15; i++) {
-        fetchData(count)
-        count++
+        let response = fetchData(countPrimary, 'mostPopular')
+
+        if (response){
+            countPrimary++
+        }
+    } 
+
+});
+
+/* Secondary */
+
+let countSecondary = 6
+
+document.getElementById('numSec').addEventListener('click', ()=>{
+    if (countSecondary <= 10){
+        let response = fetchData(countSecondary, 'secondary')
+        if(response){
+            countSecondary++
+        }
     }
+});
 
-   // console.log('hello there')
-}
+/* Others */
 
-limitIterations()
+let countOthers = 11
+
+document.getElementById('numThi').addEventListener('click', ()=>{
+    if (countOthers <= 15){
+        let response = fetchData(countOthers, 'others')
+        if(response){
+            countOthers++
+        }
+    }
+});
 
 
-
-/*
-
-limitIterations calls fetch 15 times, for each character, and each time, fetchData injects 
-
-fetchData in each Click
-
-*/
 
